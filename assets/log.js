5e74ef3d2db5e3dc7862502be5a280ba27eb272f
xumeng <1169515639@qq.com>
2019-03-05 22:18:12 +0800

    完善日志监控代码

--- assets/log.js
+++ assets/log.js
@@ -1,32 +1,40 @@
 ;(function(w){
 	var doc = document;
 	var UPLOAD_URL = 'https://xumeng.site/';
-	w.onerror = function(message, source, lineNo, colNo, error){
-	    const errorInfo = {
-	    	message: message,
-	    	source: source,
-	    	lineNumber: lineNo,
-	    	colNumber: colNo,
-	    	error: error,
-	    	type: 'Uncaught'
-	    };
-	    sendError(errorInfo);
-	};
+	// w.onerror = function(message, source, lineNo, colNo, error){
+	//     const errorInfo = {
+	//     	message: message,
+	//     	source: source,
+	//     	lineNumber: lineNo,
+	//     	colNumber: colNo,
+	//     	error: error,
+	//     	type: 'Uncaught'
+	// 	};
+	// 	console.log(error);
+	//     sendError(errorInfo);
+	// };
 
 	w.addEventListener('error', function(event){
-		console.log(event);
 		var e = event;
+		console.log(e.target);
+		const tar = {
+			id: e.target.id,
+			class: e.target.classList && [...e.target.classList].join(),
+			text: e.target.innerHTML,
+			node: e.target.nodeName
+		};
 		const errorInfo = {
 	    	message: e.message,
 	    	fileName: e.filename,
 	    	lineNumber: e.colno,
 	    	colNumber: e.lineno,
-	    	error: e.error,
+			stack: e.error && e.error.stack,
+			target:(tar),
 	    	type: e.type
-	    };
+		};
 	    sendError(errorInfo);
 	    // e.preventDefault();
-	});
+	},true);
 
 	w.addEventListener('unhandledrejection', function(event){
 		var e = event;
@@ -34,7 +42,6 @@
 	    	type: e.type,
 	    	reason: e.reason.toString()
 	    };
-	    console.log(e);
 	    sendError(errorInfo);
 	    // e.preventDefault();
 	});
@@ -46,6 +53,7 @@
 	w.addEventListener('rejectionhandled', function(event){
 		console.log(event);
 	});
+
 	Promise.prototype['catch'] = function(onRejected){
 		const reject = function (error) {
 	        var errorInfo = {
@@ -56,15 +64,15 @@
 	        if (isFunction(onRejected)) {
 	            return onRejected(error);
 	        };
-	    }
+	    };
 	    return this.then(null, reject);
-	}
+	};
 
 	function getUserInfo(){
 		var n = navigator;
 		const userInfo = {
 			cookieEnabled: n.cookieEnabled,
-			data: +new Date(),
+			data: new Date(),
 			language: n.language || n.userLanguage,
 			location: location.href,
 			platform: n.platform,
@@ -74,11 +82,11 @@
 			userAgent: n.userAgent
 		};
 		return userInfo;
-	}
+	};
 
 	function jsonS(data){
 		return typeof json === 'string' ? JSON.parse(data) : JSON.stringify(data);
-	}
+	};
 
 	function sendError(info){
 		var mes = new Image();
@@ -86,6 +94,7 @@
 		var useInfo = getUserInfo();
 
 		var sendObj = Object.assign({}, info, useInfo);
+		console.log(jsonS(sendObj).length);
 		mes.src = isQuesMark + jsonS(sendObj);
-	}
+	};
 })(window);
